9b8922bb9bba8ea0a0680d7cc888d88706daba1c
Nikita Philippov <philipesko@yandex.ru>
2019-10-12 23:34:10 +0300

    Add sprite on scene one.

--- TOA/scene_one.py
+++ TOA/scene_one.py
@@ -4,13 +4,16 @@ import os.path
 
 from main_window import CreateMainWindow
 from config import PATH_TO_RESOURCE
+from sprites import SpriteTower
 
 
 class Scene1(CreateMainWindow):
 
     def __init__(self):
         """Main method MainWindow class reinitialization for scene 1(level1)"""
-        CreateMainWindow.__init__(self)
+        super(CreateMainWindow, self).__init__()
+
+        # CreateMainWindow.__init__(self)
         self.main_menu_greets = None
         self.main_menu_greets_position = None
         self._resource_path = os.path.join(PATH_TO_RESOURCE, 'maps')
@@ -19,9 +22,14 @@ class Scene1(CreateMainWindow):
     def create(self):
         """Main method from MainWindows reinitialization"""
         # super(Scene1, self).create()
-
+        print(self.surface)
         # Set background for scene 1
+        # surface = self.surface
         self.surface.blit(self.main_menu_background, (0, 0))
+        sp = SpriteTower()
+        sp.create_tower_1(surface=self.surface)
         CreateMainWindow.show_mouse_position_with_px(self)
+
+
 
 
--- TOA/sprites.py
+++ TOA/sprites.py
@@ -1,42 +1,23 @@
 import pygame
 import os.path
 
-from config import PATH_TO_RESOURCE
-from scene_one import Scene1
+from config import PATH_TO_RESOURCE, MAIN_SIZE_FOR_WINDOW
 
 
-class SpriteTower(Scene1):
+class SpriteTower(pygame.sprite.Sprite):
     """Class for Towers"""
+
     def __init__(self):
-        Scene1.__init__(self)
+        # pygame.sprite.Sprite.__init__(self)
+        super(pygame.sprite.Sprite, self).__init__()
         self._path_to_sprite = os.path.join(PATH_TO_RESOURCE, 'tower-defense-turrets')
-
         self._image_tower1 = ['turret-1-1.png', 'turret-1-2.png', 'turret-1-3.png']
-        self.sprite_tower_1 = pygame.image.load(self._path_to_sprite, 'turret-1-1.png')
+        self.sprite_tower_1 = pygame.image.load(os.path.join(self._path_to_sprite, self._image_tower1[0]))
 
-
-    def create_tower_1(self, flag_level_tower=0, x=500, y=500):
+    @classmethod
+    def create_tower_1(self, surface, flag_level_tower=0, x=500, y=500):
         """"Create tower 1 (draw, load image, check level up for tower, x and y start coordinates)"""
         # variable "flag_level_tower" the amount depends on the number of tower levels drawn in a
         # folder with "resources". Default = 0 => 1 Level
-
-        sprite_tower_1_x = self.surface.get_width() - self.sprite_tower_1.get_wigth()
-        sprite_tower_1_y = self.surface.get_height() - self.sprite_tower_1.get_height()
-        block = []
-        block1 = self.surface((x, y))
-        block1.set_alpha(80)
-        block1.fill((255, 0, 0))
-        block1_rect = pygame.Rect(0, 0, block1.get_width(), block1.get_height())
-        block.append((block1, block1_rect))
-        self.surface.blit(block[0])
-
-
-
-
-
-
-
-
-
-
-
+        sprite_tower_1 = pygame.image.load(os.path.join(self._path_to_sprite, self._image_tower1[flag_level_tower]))
+        surface.blit(self.sprite_tower_1, (x, y))
